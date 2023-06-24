b929c5ea9eb65bacaac647ccc0be424e1ef2b8ae
Ivanka Nagornyuk <IvNagornyuk@gmail.com>
2023-06-24 22:57:08 +0300

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
-export const App = () => {
+import Form from 'components/Form';
+import Contacts from 'components/Contacts';
+import Filter from 'components/Filter';
+import { Container } from 'components/App.styled';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchContacts } from 'redux/service/getContacts';
+
+const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Container>
+      <h1>Phonebook</h1>
+      <Form />
+      <h2>Contacts</h2>
+      <Filter />
+      <Contacts />
+    </Container>
   );
 };
+
+export default App;
